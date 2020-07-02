caeff7bbfef4bfc01ac779e7848fd38af1d94001
yulia.dubenchuk@1000geeks.com <yulia.dubenchuk@1000geeks.com>
2020-07-02 20:52:21 +0300

    add graphql

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const path = require("path");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./graphql/schema");
+const resolver = require("./graphql/resolver");
 const sequelize = require("./utils/database");
-const todoRoutes = require("./routes/todo");
+const { resolve } = require("path");
 const app = express();
 const PORT = process.env.port || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use(express.json());
-app.use("/api/todo", todoRoutes);
+app.use(
+  graphqlHTTP({
+    schema: schema,
+    rootValue: resolver,
+    graphiql: true,
+  })
+);
 
 app.use((req, res, next) => {
   res.sendFile(__dirname + "/public/index.html");
--- public/app.js
+++ public/app.js
@@ -9,35 +9,71 @@ new Vue({
       todos: [],
     };
   },
-  mounted() {
-    fetch("/api/todo", {
-      method: "get",
+  created() {
+    this.$vuetify.theme.dark = true;
+    const query = `
+    query {
+      getTodos {
+        id title done createdAt updatedAt
+      }
+    }
+    `;
+    fetch("/graphql", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ query }),
     })
       .then((res) => res.json())
-      .then((todos) => {
-        this.todos = todos;
-      })
-      .catch((e) => console.log(e));
+      .then((response) => {
+        this.todos = response.data.getTodos;
+      });
   },
   methods: {
     addTodo() {
       const title = this.todoTitle.trim();
       if (!title) return;
-      fetch("/api/todo", {
+
+      const query = `
+        mutation {
+          createTodo(todo: { title: "${title}" }) {
+            id title done createdAt updatedAt
+          }
+        }
+      `;
+
+      fetch("/graphql", {
         method: "post",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ title }),
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({ query }),
       })
         .then((res) => res.json())
-        .then(({ todo }) => {
+        .then((response) => {
+          const todo = response.data.createTodo;
           this.todos.push(todo);
           this.todoTitle = "";
         })
         .catch((e) => console.log(e));
     },
     removeTodo(id) {
-      fetch("/api/todo/" + id, {
-        method: "delete",
+      const query = `
+      mutation {
+        deleteTodo(id: "${id}")
+      }
+      `;
+
+      fetch("/graphql", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({ query }),
       })
         .then(() => {
           this.todos = this.todos.filter((t) => t.id !== id);
@@ -47,17 +83,32 @@ new Vue({
         });
     },
     completeTodo(id) {
-      fetch("/api/todo/" + id, {
-        method: "put",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ done: true }),
+      const query = `
+        mutation {
+          completeTodo(id: "${id}") {
+            updatedAt
+          }
+        }
+      `;
+
+      fetch("/graphql", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({ query }),
       })
         .then((res) => res.json())
-        .then(({ todo }) => {
-          const idx = this.todos.findIndex((t) => t.id === todo.id);
-          this.todos[idx].updatedAt = todo.updatedAt;
+        .then((response) => {
+          const idx = this.todos.findIndex((t) => t.id === id);
+          this.todos[idx].updatedAt = response.data.completeTodo.updatedAt;
         })
         .catch((e) => console.log(e));
+    },
+    changeTheme() {
+      const dark = (this.isDark = !this.isDark);
+      this.$vuetify.theme.dark = dark;
     },
   },
   filters: {
@@ -76,10 +127,7 @@ new Vue({
         options.minute = "2-digit";
         options.second = "2-digit";
       }
-      return new Intl.DateTimeFormat("en-En", options).format(new Date(value));
+      return new Intl.DateTimeFormat("en-En", options).format(new Date(+value));
     },
-  },
-  created() {
-    this.$vuetify.theme.dark = true;
   },
 });
